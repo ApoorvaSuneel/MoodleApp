fe1e615f5afb1919beca237ccb6f7b38ae2151d3
ApoorvaSuneel <apoorvasuneel3@gamil.com>
2016-02-17 16:29:17 +0530

    second activity added

--- app/src/main/java/com/example/user/moodleapp/MainActivity0.java
+++ app/src/main/java/com/example/user/moodleapp/MainActivity0.java
@@ -1,5 +1,6 @@
 package com.example.user.moodleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,20 @@ public class MainActivity0 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main0);
+        Thread timer = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent myIntent = new Intent(
+                            MainActivity0.this,MainActivity1.class);
+                    startActivity(myIntent);
+                }
+            }
+        };
+        timer.start();
     }
 }
