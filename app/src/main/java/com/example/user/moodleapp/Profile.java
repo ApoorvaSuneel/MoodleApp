852bd5dde1a84cdfc10fd82714ff353186c876eb
ApoorvaSuneel <apoorvasuneel3@gamil.com>
2016-02-22 01:25:52 +0530

    uniform the  master

--- app/src/main/java/com/example/user/moodleapp/Profile.java
+++ app/src/main/java/com/example/user/moodleapp/Profile.java
@@ -12,16 +12,15 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        b1=(Button)findViewById(R.id.assign);
+        b1=(Button)findViewById(R.id.grades);
         b2=(Button)findViewById(R.id.course);
-        b3=(Button)findViewById(R.id.thread);
         b4=(Button)findViewById(R.id.notify);
         b1.setOnClickListener(new View.OnClickListener()
         {
 
             public void onClick(View v) {
                 Intent myIntent = new Intent(
-                        Profile.this,Assignments.class);
+                        Profile.this,Grades.class);
                 startActivity(myIntent);
             }
         });
